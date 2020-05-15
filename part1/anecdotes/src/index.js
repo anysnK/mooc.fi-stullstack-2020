c5e3b4dc1934e000b88e504536d0c04e28d12955
Jost Oppenheimer <jost.oppenheimer@gmail.com>
2020-05-15 14:11:02 +0200

    finished part1: anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
-const Anecdote = ({ anecdotes, selected }) => {
-
+const Anecdote = ({ anecdotes, selected, votes }) => {
+  console.log(selected)
   return (
-    <div>
-      {anecdotes[selected]}
-    </div>
+    <>
+      <div>
+        <>{anecdotes[selected]}</>
+      </div>
+      <div>
+        <>score: {votes[selected]} </>
+      </div>
+    </>
   )
 }
 
@@ -22,12 +27,35 @@ const Button = ({ tag, handleClick }) => {
 const App = (props) => {
   const [selected, setSelected] = useState(0)
 
+  const [votes, setVotes] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf, 0))
+  console.log(votes)
+
+  const onVote = (selected) => {
+    const helper = [...votes]
+    helper[selected]++
+    setVotes(helper)
+  }
+
+  const findMax = (array) => {
+    let index=0;
+    let max=array[0]
+    for(let i = 1; i<array.length; i++){
+      if(max < array[i]){
+        max = array[i]
+        index=i
+      }
+    }
+    return index
+  }
+
   return (
 
     <div>
-    <Anecdote anecdotes={anecdotes} selected={selected} />
+      <Anecdote anecdotes={anecdotes} selected={selected} votes={votes} />
+      <Button tag='next' handleClick={() => { setSelected(Math.floor(Math.random() * 6)) }} />
 
-    <Button tag='next' handleClick={() => { setSelected(Math.floor(Math.random() * 6)) }} />
+      <Button tag='vote' handleClick={() => { onVote(selected) }} />
+      <Button tag='best' handleClick={() => { setSelected(findMax(votes))}} />
     </div>
   )
 }
